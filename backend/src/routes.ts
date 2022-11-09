7c9eab7e64c01e357d4017a7740db8139c700890
Miguel Arcanjo <miguel.arcanjo@sidebit.dev>
2022-11-08 21:53:17 -0300

    Tratando error 400, 500 e Cors

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -3,7 +3,8 @@ import { Router, Request, Response } from 'express';
 const router = Router();
 
 router.get('/teste', (req: Request, res: Response) => {
-    return res.json({ Syspizza: "Online!"})
+   return res.json({ Syspizza: "Online!"})
+   // throw new Error('Erro ao fazer essa requisição')
 });
 
 export { router };

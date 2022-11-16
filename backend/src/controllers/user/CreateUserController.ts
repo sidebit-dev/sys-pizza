b44f375c809666f286fa57214a8d6695b92c6c9b
Miguel Arcanjo <miguel.arcanjo@sidebit.dev>
2022-11-15 23:01:27 -0300

    Criando estrutura de controller e service para user

---
+++ backend/src/controllers/user/CreateUserController.ts
@@ -0,0 +1,22 @@
+import {Request, response, Response} from 'express';
+import { CreateUserService } from '../../services/user/CreateUserService';
+
+class CreateUserController{
+    async handle(req: Request, res: Response){
+        // console.log(req.body);
+        const { name, email, password } = req.body;
+
+        const createUserService = new CreateUserService();
+
+        const user = await createUserService.execute({
+            name,
+            email,
+            password
+        });
+
+        // return res.json({ ok: true})
+        return res.json(user)
+    }
+}
+
+export { CreateUserController }

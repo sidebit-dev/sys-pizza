4c1021b94515a8780d84c8fc01643e9e71dd9657
Miguel Arcanjo <miguel.arcanjo@sidebit.dev>
2022-11-21 00:48:03 -0300

    Cadastrando categorias

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -4,7 +4,9 @@ import { CreateUserController } from './controllers/user/CreateUserController';
 import { AuthUserController } from './controllers/user/AuthUserController';
 import { DetailUserController } from './controllers/user/DetailUserController';
 
-import { isAuthenticated } from './middlewares/isAuthenticated'
+import { CreateCategoryController } from './controllers/category/CreateCategoryController';
+
+import { isAuthenticated } from './middlewares/isAuthenticated';
 
 const router = Router();
 
@@ -18,5 +20,8 @@ router.post('/users', new CreateUserController().handle);
 router.post('/session', new AuthUserController().handle);
 
 router.get('/me', isAuthenticated, new DetailUserController().handle);
+
+//-- ROTAS CATEGORY
+router.post('/category', isAuthenticated, new CreateCategoryController().handle);
 
 export { router };   

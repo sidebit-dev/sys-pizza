d8b09aba8da0ab3dbf1ecae8123c9e03f4f9ba34
Miguel Arcanjo <miguel.arcanjo@sidebit.dev>
2022-11-23 22:34:18 -0300

    Listando categorias

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -5,6 +5,7 @@ import { AuthUserController } from './controllers/user/AuthUserController';
 import { DetailUserController } from './controllers/user/DetailUserController';
 
 import { CreateCategoryController } from './controllers/category/CreateCategoryController';
+import { ListCategoryController } from './controllers/category/ListCategoryController';
 
 import { isAuthenticated } from './middlewares/isAuthenticated';
 
@@ -23,5 +24,7 @@ router.get('/me', isAuthenticated, new DetailUserController().handle);
 
 //-- ROTAS CATEGORY
 router.post('/category', isAuthenticated, new CreateCategoryController().handle);
+
+router.get('/category', isAuthenticated, new ListCategoryController().handle);
 
 export { router };   

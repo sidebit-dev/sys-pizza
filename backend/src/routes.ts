cd1d569a693fba3c317c50d0e2c7a54bc91332c9
Miguel Arcanjo <miguel.arcanjo@sidebit.dev>
2022-12-04 23:38:51 -0300

    Detalhes do pedido

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -17,7 +17,9 @@ import { RemoveOrderController } from './controllers/order/RemoveOrderController
 import { AddItemController } from './controllers/order/AddItemController';
 import { RemoveItemController } from './controllers/order/RemoveItemController';
 import { SendOrderController } from './controllers/order/SendOrderController';
-import { ListOrdersController } from './controllers/order/ListOrdersController'; 
+
+import { ListOrdersController } from './controllers/order/ListOrdersController';
+import { DetailOrderController } from './controllers/order/DetailOrderController'; 
 
 import { isAuthenticated } from './middlewares/isAuthenticated';
 
@@ -54,10 +56,11 @@ router.post('/order', isAuthenticated, new CreateOrderController().handle);
 router.delete('/order', isAuthenticated, new RemoveOrderController().handle);
 
 router.post('/order/add', isAuthenticated, new AddItemController().handle);
-router.delete('/order/remove', isAuthenticated, new RemoveItemController().handle)
+router.delete('/order/remove', isAuthenticated, new RemoveItemController().handle);
 
 router.put('/order/send', isAuthenticated, new SendOrderController().handle);
 
-router.get('/orders', isAuthenticated, new ListOrdersController().handle)
+router.get('/orders', isAuthenticated, new ListOrdersController().handle);
+router.get('/order/detail', isAuthenticated, new DetailOrderController().handle);
 
 export { router };   

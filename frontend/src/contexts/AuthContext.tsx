e5afca185dff628d55cae6b5098efc251580ee8b
Miguel Arcanjo <miguel.arcanjo@sidebit.dev>
2022-12-22 23:48:50 -0300

    Cadastra usuario

--- frontend/src/contexts/AuthContext.tsx
+++ frontend/src/contexts/AuthContext.tsx
@@ -10,6 +10,7 @@ type AuthContextData = {
   isAuthenticated: boolean;
   signIn: (credentials: SignInProps) => Promise<void>;
   signOut: () => void;
+  signUp: (credentials: SignUpProps) => Promise<void>;
 };
 
 type UserProps = {
@@ -22,6 +23,12 @@ type SignInProps = {
   email: string;
   password: string;
 };
+
+type SignUpProps = {
+  name: string;
+  email: string;
+  password: string;
+}
 
 type AuthProviderProps = {
   children: ReactNode;
@@ -76,8 +83,26 @@ export function AuthProvider({ children }: AuthProviderProps) {
     }
   }
 
+  async function signUp({ name, email, password }: SignUpProps){
+    try {
+
+      const response = await api.post('/users', {
+        name,
+        email,
+        password
+      })
+
+      console.log("CADASTRADO COM SUCESSO!");
+
+      Router.push('/')
+      
+    } catch (err) {
+      console.log("Erro ao cadastrar", err);
+    }
+  }
+
   return (
-    <AuthContext.Provider value={{ user, isAuthenticated, signIn, signOut }}>
+    <AuthContext.Provider value={{ user, isAuthenticated, signIn, signOut, signUp }}>
       {children}
     </AuthContext.Provider>
   );

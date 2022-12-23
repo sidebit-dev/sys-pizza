46dd44d05f55afa2bcbeaf050f90f808041812ca
Miguel Arcanjo <miguel.arcanjo@sidebit.dev>
2022-12-22 21:19:15 -0300

    Fazendo login

--- frontend/src/contexts/AuthContext.tsx
+++ frontend/src/contexts/AuthContext.tsx
@@ -1,54 +1,84 @@
-import { createContext, ReactNode, useState } from 'react';
+import { createContext, ReactNode, useState } from "react";
 
-import { destroyCookie } from 'nookies';
-import Router from 'next/router';
+import { api } from "../services/apiClient";
+
+import { destroyCookie, setCookie, parseCookies } from "nookies";
+import Router from "next/router";
 
 type AuthContextData = {
-    user: UserProps;
-    isAuthenticated: boolean;
-    signIn: (credentials: SignInProps) => Promise<void>;
-    signOut: () => void;
-    }
+  user: UserProps;
+  isAuthenticated: boolean;
+  signIn: (credentials: SignInProps) => Promise<void>;
+  signOut: () => void;
+};
 
 type UserProps = {
-    id: string;
-    name: string;
-    email: string;
-}
+  id: string;
+  name: string;
+  email: string;
+};
 
 type SignInProps = {
-    email: string;
-    password: string;
-}
+  email: string;
+  password: string;
+};
 
 type AuthProviderProps = {
-   children: ReactNode;
+  children: ReactNode;
+};
+
+export const AuthContext = createContext({} as AuthContextData);
+
+export function signOut() {
+  try {
+    destroyCookie(undefined, "@nextauth.token");
+    Router.push("/");
+  } catch {
+    console.log("erro ao deslogar");
+  }
 }
 
+export function AuthProvider({ children }: AuthProviderProps) {
+  const [user, setUser] = useState<UserProps>();
+  const isAuthenticated = !!user;
 
-export const AuthContext = createContext({} as AuthContextData)
-
-export function signOut(){
+  async function signIn({ email, password }: SignInProps) {
+    //    console.log("DADOS PARA LOGAR", email);
+    //    console.log("SENHA", password);
     try {
-        destroyCookie(undefined, '@nextauth.token')
-        Router.push('/')
-    } catch{
-        console.log('erro ao deslogar');
+      const response = await api.post("/session", {
+        email,
+        password,
+      });
+      //console.log(response.data);
+
+      const { id, name, token } = response.data;
+
+      setCookie(undefined, "@nextauth.token", token, {
+        maxAge: 60 * 60 * 24 * 30, // Expirar em 1 mes
+        path: "/", // quais caminhos terao acesso ao cookie
+      });
+
+      setUser({
+        id,
+        name,
+        email,
+      });
+
+      //Passar para proximas requisicoes o nosso token
+      api.defaults.headers['Authrization'] =  `Bearer ${token}`
+
+      //Redirecionar o user para /dashboard
+      Router.push('/dashboard');
+      
+    } catch (err) {
+      console.log("ERRO AO ACESSAR ", err);
     }
-}
+  }
 
-export function AuthProvider({ children }: AuthProviderProps){
-    const [user, setUser] = useState<UserProps>()
-    const isAuthenticated = !!user;
-
-    async function signIn({ email, password }: SignInProps){
-       console.log("DADOS PARA LOGAR", email);
-       console.log("SENHA", password);
-    }
-
-    return(
-        <AuthContext.Provider value={{ user, isAuthenticated, signIn, signOut }}> 
-            {children}
-        </AuthContext.Provider>
-    )    
+  return (
+    <AuthContext.Provider value={{ user, isAuthenticated, signIn, signOut }}>
+      {children}
+    </AuthContext.Provider>
+  );
 }

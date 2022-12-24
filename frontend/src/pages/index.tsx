5738d95b69007045cf58375724f9b227e271128d
Miguel Arcanjo <miguel.arcanjo@sidebit.dev>
2022-12-24 01:56:29 -0300

    Rotas privadas - parte 1

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -14,6 +14,8 @@ import { AuthContext } from '../contexts/AuthContext';
 
 import Link from "next/link";
 
+import { canSSRGuest } from '../utils/canSSRGuest';
+
 export default function Home() {
 const { signIn } = useContext(AuthContext);
 
@@ -81,3 +83,10 @@ async function handleLogin(event: FormEvent){
 </div>
 </>  )
 }
+
+
+export const getServerSideProps = canSSRGuest(async (ctx) => {
+  return {
+    props: {}
+  }
+})

3fcb6442ff601b2f0c6d757ec161035f6b8c6e71
Miguel Arcanjo <miguel.arcanjo@sidebit.dev>
2022-12-24 18:23:21 -0300

    Permace login

--- frontend/src/contexts/AuthContext.tsx
+++ frontend/src/contexts/AuthContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, ReactNode, useState } from "react";
+import { createContext, ReactNode, useState, useEffect } from "react";
 
 import { api } from "../services/apiClient";
 
@@ -50,6 +50,25 @@ export function signOut() {
 export function AuthProvider({ children }: AuthProviderProps) {
   const [user, setUser] = useState<UserProps>();
   const isAuthenticated = !!user;
+
+  useEffect(() => {
+  //  alert("TESTE"); Tentar pegar o cookie
+  const { '@nextauth.token': token } = parseCookies();  
+
+  if(token){
+    api.get('/me').then(response => {
+      const { id, name, email } = response.data;
+
+      setUser({
+        id,
+        name,
+        email
+      })
+    })
+  }
+
+
+  }, [])
 
   async function signIn({ email, password }: SignInProps) {
     //    console.log("DADOS PARA LOGAR", email);

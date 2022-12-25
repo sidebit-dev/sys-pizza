90a3cb33fa883ccc8eeba52d72d979b55937723c
Miguel Arcanjo <miguel.arcanjo@sidebit.dev>
2022-12-25 10:49:08 -0300

    Pagina Categoria

---
+++ frontend/src/pages/category/index.tsx
@@ -0,0 +1,43 @@
+import { useState, FormEvent } from 'react';
+import Head from "next/head";
+import {Header} from '../../components/Header';
+import styles from './styles.module.scss';
+
+export default function Category(){
+    const [name, setName]= useState('');
+
+    async function handleRegister(event: FormEvent){
+        event.preventDefault(); //Para não atualizar a página
+
+        alert("CATEGORIA "+ name);
+    }
+
+    return(
+       <>
+       <Head>
+        <title>Nova categoria - Sys Pizza</title>
+       </Head>
+       <div>
+        <Header/>
+
+        <main className={styles.container}>
+            <h1>Cadastrar categrias</h1>
+
+            <form className={styles.form} onSubmit={handleRegister}>
+                <input 
+                type="text" 
+                placeholder="Digite o nome da categoria.."
+                className={styles.input}
+                value={name}
+                onChange={ (e) => setName(e.target.value) }
+                />
+
+                <button className={styles.buttonAdd} type="submit">
+                    Cadastrar
+                </button>
+            </form>
+        </main>
+       </div>
+       </> 
+    )
+}

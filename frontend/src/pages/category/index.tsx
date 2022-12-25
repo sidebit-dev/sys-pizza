e5ebb1700818f82cd99ac310a4b9a418504ff680
Miguel Arcanjo <miguel.arcanjo@sidebit.dev>
2022-12-25 12:59:03 -0300

    Registrando Categoria

--- frontend/src/pages/category/index.tsx
+++ frontend/src/pages/category/index.tsx
@@ -3,13 +3,26 @@ import Head from "next/head";
 import {Header} from '../../components/Header';
 import styles from './styles.module.scss';
 
+import { setupAPIClient } from '../../services/api';
+import { toast } from 'react-toastify';
+
 export default function Category(){
     const [name, setName]= useState('');
 
     async function handleRegister(event: FormEvent){
         event.preventDefault(); //Para não atualizar a página
+        // alert("CATEGORIA "+ name);
+        if(name === ''){
+            return;
+        }
 
-        alert("CATEGORIA "+ name);
+        const apiClient = setupAPIClient();
+        await apiClient.post('/category', {
+            name: name
+        })
+
+        toast.success('Categoria cadastrada com sucesso!')
+        setName('');
     }
 
     return(

e5c039ffbc91e91ac513f9f1b6bedc8eb2e109f1
Miguel Arcanjo <miguel.arcanjo@sidebit.dev>
2022-12-25 16:14:37 -0300

    Componete preview

--- frontend/src/pages/product/index.tsx
+++ frontend/src/pages/product/index.tsx
@@ -1,61 +1,98 @@
+import { ChangeEvent, useState } from "react";
 import Head from "next/head";
-import {Header} from '../../components/Header';
-import styles from './styles.module.scss';
+import { Header } from "../../components/Header";
+import styles from "./styles.module.scss";
 
-import { canSSRAuth } from '../../utils/canSSAuth';
+import { canSSRAuth } from "../../utils/canSSAuth";
 
-export default function Product(){
-    return(
-        <>
-            <Head>
-                <title>Novo Produto - Sys Pizza</title>
-            </Head>
-            <div>
-                <Header/>
-                <main className={styles.container}>
-                <h1>Novo produto</h1>
+import { FiUpload } from 'react-icons/fi';
 
-                <form className={styles.form}>
+export default function Product() {
 
+    const [avatarUrl, setAvatarUrl] = useState('');
+    const [imageAvatar, setImageAvatar] = useState(null);
 
-                    <select>
-                        <option>
-                            Bebida
-                        </option>
-                        <option>
-                            Pizzas
-                        </option>
-                    </select>
+    function handleFile(e: ChangeEvent<HTMLInputElement>){       
+        //console.log(e.target.files);
+       
+        if(!e.target.files){
+            return;
+        }
 
-                    <input
-                    type={"text"}
-                    placeholder="Digite o nome do produto"
-                    className={styles.input}
+        const image = e.target.files[0];
+
+        if(!image){
+            return;
+        }
+
+        if(image.type === 'image/jpeg' || image.type === 'image/png'){
+            setImageAvatar(image);
+            setAvatarUrl(URL.createObjectURL(e.target.files[0]));
+        }
+    }
+
+  return (
+    <>
+      <Head>
+        <title>Novo Produto - Sys Pizza</title>
+      </Head>
+      <div>
+        <Header />
+        <main className={styles.container}>
+          <h1>Novo produto</h1>
+
+          <form className={styles.form}>
+
+            <label className={styles.labelAvatar}>
+                <span>
+                    <FiUpload size={30} color='#FFF' />
+                </span>
+                <input type="file" accept="image/png, image/jpeg" onChange={handleFile} />
+                {avatarUrl && (
+                    <img 
+                        className={styles.preview}
+                        src={avatarUrl} 
+                        alt="Foto do Produto" 
+                        width={250}
+                        height={250}
                     />
+                )}
+            </label>
 
-<input
-                    type={"text"}
-                    placeholder="Preço do produto"
-                    className={styles.input}
-                    />
+            <select>
+              <option>Bebida</option>
+              <option>Pizzas</option>
+            </select>
 
-                    <textarea
-                    placeholder="Descreva seu produto.."
-                    className={styles.input}
-                    />
+            <input
+              type={"text"}
+              placeholder="Digite o nome do produto"
+              className={styles.input}
+            />
 
-                    <button className={styles.buttonAdd} type="submit">
-                        Cadastrar
-                    </button>
-                </form>
-                </main>
-            </div>
-        </>
-    )
+            <input
+              type={"text"}
+              placeholder="Preço do produto"
+              className={styles.input}
+            />
+
+            <textarea
+              placeholder="Descreva seu produto.."
+              className={styles.input}
+            />
+
+            <button className={styles.buttonAdd} type="submit">
+              Cadastrar
+            </button>
+          </form>
+        </main>
+      </div>
+    </>
+  );
 }
 
 export const getServerSideProps = canSSRAuth(async (ctx) => {
-    return {
-        props: {}
-    }
-})
+  return {
+    props: {},
+  };
+});

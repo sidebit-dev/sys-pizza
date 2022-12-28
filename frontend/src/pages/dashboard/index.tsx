218e31ba9f39c2c04f24f2466d78ede9b4ccd96a
Miguel Arcanjo <miguel.arcanjo@sidebit.dev>
2022-12-28 20:45:08 -0300

    Cadastrando dashboard

--- frontend/src/pages/dashboard/index.tsx
+++ frontend/src/pages/dashboard/index.tsx
@@ -1,7 +1,9 @@
 import { canSSRAuth } from "../../utils/canSSAuth";
 import Head from 'next/head';
+import styles from './styles.module.scss';
 
 import { Header } from '../../components/Header';
+import { FiRefreshCcw } from 'react-icons/fi';
 
 export default function Dashboard(){
     return(
@@ -11,7 +13,26 @@ export default function Dashboard(){
         </Head>
         <div>
             <Header />
-            <h1>Painel</h1>
+            
+            <main className={styles.container}>
+                <div className={styles.containerHeader}>
+                    <h1>Últimos Pedidos</h1>
+                    <button>
+                        <FiRefreshCcw size={25} color="#3FFFA3"/>
+                    </button>
+                </div>
+
+                <article className={styles.listOrders}>
+
+                    <section className={styles.orderItem}>
+                        <button>
+                           <div className={styles.tag}></div>
+                           <span>Mesa 30</span>
+                        </button>
+                    </section>
+
+                </article>
+            </main>
         </div>
         </>
     )

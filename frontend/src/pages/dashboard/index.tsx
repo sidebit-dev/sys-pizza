4622644019084f8c27a46fd7b9de6c02b73a876b
Miguel Arcanjo <miguel.arcanjo@sidebit.dev>
2022-12-29 17:45:53 -0300

    Concluindo pedido

--- frontend/src/pages/dashboard/index.tsx
+++ frontend/src/pages/dashboard/index.tsx
@@ -69,6 +69,27 @@ export default function Dashboard({ orders }: HomeProps){
         setModalVisible(true);
     }
 
+    async function handleFinishItem(id: string){
+        // alert("FECHAR PEDIDO " + id)
+        const apiClient = setupAPIClient();
+        await apiClient.put('/order/finish', {
+           order_id: id,
+        })
+
+        const response = await apiClient.get('/orders');
+
+        setOrderList(response.data);
+        setModalVisible(false);
+    }
+
+async function handleRefreshOrders(){
+    const apiClient = setupAPIClient();
+
+    const response = await apiClient.get('/orders');
+
+    setOrderList(response.data);
+}
+
     Modal.setAppElement('#__next');
 
     return(
@@ -82,12 +103,18 @@ export default function Dashboard({ orders }: HomeProps){
             <main className={styles.container}>
                 <div className={styles.containerHeader}>
                     <h1>Últimos Pedidos</h1>
-                    <button>
+                    <button onClick={handleRefreshOrders}>
                         <FiRefreshCcw size={25} color="#3FFFA3"/>
                     </button>
                 </div>
 
                 <article className={styles.listOrders}>
+
+                    {orderList.length === 0 && (
+                        <span className={styles.emptyList}>
+                            Nenhum padido aberto foi encontrado...
+                        </span>
+                    )}
 
                     {orderList.map( item => (
                     <section key={item.id} className={styles.orderItem}>
@@ -106,6 +133,7 @@ export default function Dashboard({ orders }: HomeProps){
                 isOpen={modalVisible}
                 onRequestClose={handleCloseModal}
                 order={modalItem}
+                handleFinishOrder={ handleFinishItem }
                 />
             )}
             

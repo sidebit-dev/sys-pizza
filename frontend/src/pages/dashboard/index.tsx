a5bf546219898b04314df96f9fdebb31d61f807c
Miguel Arcanjo <miguel.arcanjo@sidebit.dev>
2022-12-29 09:28:42 -0300

    Busca orders - pedidos

--- frontend/src/pages/dashboard/index.tsx
+++ frontend/src/pages/dashboard/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { canSSRAuth } from "../../utils/canSSAuth";
 import Head from 'next/head';
 import styles from './styles.module.scss';
@@ -5,7 +6,28 @@ import styles from './styles.module.scss';
 import { Header } from '../../components/Header';
 import { FiRefreshCcw } from 'react-icons/fi';
 
-export default function Dashboard(){
+import { setupAPIClient } from '../../services/api';
+
+type OrderProps = {
+    id: string;
+    table: string | number;
+    status: boolean;
+    draft: boolean;
+    name: string | null;
+}
+
+interface HomeProps{
+    orders: OrderProps[];
+}
+
+export default function Dashboard({ orders }: HomeProps){
+
+    const [orderList, setOrderList] = useState(orders || []);
+
+    function handleOpenModalView(id: string){
+        alert("TESTE " + id );
+    }
+
     return(
         <>
         <Head>
@@ -24,12 +46,14 @@ export default function Dashboard(){
 
                 <article className={styles.listOrders}>
 
-                    <section className={styles.orderItem}>
-                        <button>
-                           <div className={styles.tag}></div>
-                           <span>Mesa 30</span>
+                    {orderList.map( item => (
+                    <section key={item.id} className={styles.orderItem}>
+                        <button onClick={ () => handleOpenModalView(item.id) }>
+                            <div className={styles.tag}></div>
+                            <span>Mesa {item.table}</span>
                         </button>
                     </section>
+                    ))}
 
                 </article>
             </main>
@@ -39,8 +63,15 @@ export default function Dashboard(){
 }
 
 export const getServerSideProps = canSSRAuth(async (ctx) => {
+    const apiClient = setupAPIClient(ctx);
+
+    const response = await apiClient.get('/orders');
+
+    // console.log(response.data);
 
     return {
-        props: {}
+        props: {
+            orders: response.data
+        }
     }
 });
